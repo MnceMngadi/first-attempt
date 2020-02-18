17c94af0a6e813a62701170bdc2a2a2cc53ff716
sinethemba <poni.sneh@gmail.com>
2020-02-18 15:52:16 +0200

    First Commit

---
+++ src/main/java/FirstApplication.java
@@ -0,0 +1,13 @@
+public class FirstApplication {
+    public static void main (String [] args){
+        /*
+        String str = "First application";
+        System.out.println(str);
+
+         */
+
+        System.out.println("First Application");
+    }
+
+
+}
